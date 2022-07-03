3c6aee7a042cbd33a56fe879eb431a9ab7939685
Alexander Horn <alexhorn@users.noreply.github.com>
2022-07-03 15:35:27 +0200

    Replace spaces in tokens

--- src/main/kotlin/astminer/storage/path/Code2VecPathStorage.kt
+++ src/main/kotlin/astminer/storage/path/Code2VecPathStorage.kt
@@ -27,9 +27,9 @@ class Code2VecPathStorage(
     }
 
     private fun storePathContext(pathContext: PathContext): PathContextId {
-        val startTokenId = pathContext.startToken.replace(",", "comma")
+        val startTokenId = pathContext.startToken.replace(" ", "space").replace(",", "comma")
         tokensMap.record(startTokenId)
-        val endTokenId = pathContext.endToken.replace(",", "comma")
+        val endTokenId = pathContext.endToken.replace(" ", "space").replace(",", "comma")
         tokensMap.record(endTokenId)
         val pathId = pathContext.orientedNodeTypes.sumOf { "${it.typeLabel} ${it.direction}".hashCode() }
         pathsMap.record(pathId)
